c4d17ed3fbc1021c0626b8aa04dae1d9b7c38406
rhl120 <rhl120@protonmail.com>
2022-06-11 10:19:00 +0200

    Use an array instead of a map because arrays are ordered

--- applets/applets.go
+++ applets/applets.go
@@ -3,15 +3,16 @@ package applets
 import "fmt"
 
 type Applet struct {
+	name           string
 	UpdateInterval uint
 	Enabled        bool
 	Function       func(...interface{}) (string, error)
 }
 
-var Applets map[string]Applet = map[string]Applet{
-	"battery": {UpdateInterval: 120, Enabled: true, Function: batteryApplet},
-	"date":    {UpdateInterval: 60, Enabled: true, Function: dateApplet},
-	"time":    {UpdateInterval: 60, Enabled: true, Function: timeApplet},
+var Applets []Applet = []Applet{
+	{UpdateInterval: 120, Enabled: true, Function: batteryApplet},
+	{UpdateInterval: 60, Enabled: true, Function: dateApplet},
+	{UpdateInterval: 60, Enabled: true, Function: timeApplet},
 }
 
 func PrintApplets() {
